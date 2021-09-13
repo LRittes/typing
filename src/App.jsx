2b1338cb0e297ee8313de72285d47bf1d8e40fc3
lrittes <leandro_rittes@hotmail.com>
2021-09-13 13:29:56 -0300

    initial structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+    return (
+        <div className="container">
+            <div className="valid-keys">
+                <span className="matched">emer</span>
+                <span className="remainder">son</span>
+            </div>
+            <div className="typed-keys">asdemerxacd</div>
+            <div className="completed-words">
+                <ol>
+                    <li>cidade</li>
+                    <li>carro</li>
+                    <li>puta</li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default App;
